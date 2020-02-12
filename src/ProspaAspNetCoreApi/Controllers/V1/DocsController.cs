f3300ab08a525374966379f6e81df755193a48d9
al_hardy <al_hardy@live.com.au>
2020-02-12 11:25:34 +1100

    health endpoints pipeline conflicting with api versioning i.e. the middleware runs after version and failed because no version set

--- src/ProspaAspNetCoreApi/Controllers/V1/DocsController.cs
+++ src/ProspaAspNetCoreApi/Controllers/V1/DocsController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
